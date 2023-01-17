6be9a0eaaa029012fbc540f16e017ce4a72a8498
François COSSON <contact@fracos.fr>
2023-01-17 12:30:48 +0100

    Change for debug

--- lib/src/request/directus_response.dart
+++ lib/src/request/directus_response.dart
@@ -15,8 +15,8 @@ class DirectusResponse {
       this.url, String body, this.method, bool debugMode, bool parseJson)
       : rawData = body {
     try {
-      _data = parseJson ? jsonDecode(rawData) : {"data": rawData};
       if (debugMode) print(data);
+      _data = parseJson ? jsonDecode(rawData) : {"data": rawData};
     } catch (e) {
       throw DirectusErrorHttpJsonException();
     }
