fc1dcc752546f2a7b189abddde87baf76290766c
François COSSON <contact@fracos.fr>
2023-04-05 13:11:29 +0200

    update

--- lib/src/request/directus_filter.dart
+++ lib/src/request/directus_filter.dart
@@ -1,32 +1,5 @@
 import 'dart:convert';
 
-enum FilterKey {
-  equal,
-  doesntEqual,
-  lessThan,
-  lessThanOrEqualTo,
-  greaterThan,
-  greaterThanOrEqualTo,
-  isntNull,
-  contains,
-  coesntContain,
-  startsWith,
-  doesntStartWith,
-  endsWith,
-  doesntEndWith,
-  isBetween,
-  isntBetween,
-  isEmpty,
-  isntEmpty,
-  intersects,
-  doesntIntersect,
-  intersectsBoundingBox,
-  doesntIntersectBoundingBox,
-  currentUser,
-  currentRole,
-  now,
-}
-
 abstract class DirectusFilterContructor {
   DirectusFilterContructor(this.key);
   final String key;
@@ -58,70 +31,6 @@ abstract class DirectusFilterContructor {
     return {column: value};
   }
 }
-
-/*class Filter extends DirectusFilterContructor {
-  Filter(this.column, this.key, this.value);
-  final FilterKey key;
-  final dynamic value;
-  final String column;
-
-  Map get map => {
-        column: {filterToString(key): parseData(value)}
-      };
-
-  static String filterToString(FilterKey key) {
-    switch (key) {
-      case FilterKey.equal:
-        return "_eq";
-      case FilterKey.doesntEqual:
-        return "_neq";
-      case FilterKey.lessThan:
-        return "_lt";
-      case FilterKey.lessThanOrEqualTo:
-        return "_lte";
-      case FilterKey.greaterThan:
-        return "_gt";
-      case FilterKey.greaterThanOrEqualTo:
-        return "_gte";
-      case FilterKey.isntNull:
-        return "_nnuul";
-      case FilterKey.contains:
-        return "_contains";
-      case FilterKey.coesntContain:
-        return "_ncontains";
-      case FilterKey.startsWith:
-        return "_starts_with";
-      case FilterKey.doesntStartWith:
-        return "_nstarts_with";
-      case FilterKey.endsWith:
-        return "_ends_with";
-      case FilterKey.doesntEndWith:
-        return "_nends_with";
-      case FilterKey.isBetween:
-        return "_between";
-      case FilterKey.isntBetween:
-        return "_nbetween";
-      case FilterKey.isEmpty:
-        return "_empty";
-      case FilterKey.isntEmpty:
-        return "_nempty";
-      case FilterKey.intersects:
-        return "_intersects";
-      case FilterKey.doesntIntersect:
-        return "_nintersects";
-      case FilterKey.intersectsBoundingBox:
-        return "_intersects_bbox";
-      case FilterKey.doesntIntersectBoundingBox:
-        return "_nintersects_bbox";
-      case FilterKey.currentUser:
-        return "\$CURRENT_USER";
-      case FilterKey.currentRole:
-        return "\$CURRENT_ROLE";
-      case FilterKey.now:
-        return "\$NOW";
-    }
-  }
-}*/
 
 class Filter {
   static DirectusFilterListFilter or(List<DirectusFilterContructor> items) =>
@@ -179,7 +88,7 @@ class DirectusFilterList extends DirectusFilterContructor {
       obj.add(parseData(i));
     }
 
-    return mapParser(column, {key: obj.toString()});
+    return mapParser(column, {key: jsonEncode(obj)});
   }
 }
 
